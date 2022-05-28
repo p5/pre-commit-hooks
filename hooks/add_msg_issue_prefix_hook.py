de81c3dcace2553ee3140f634aefd8afbc99ece3
Robert <robertsturla@outlook.com>
2022-05-28 01:22:54 +0100

    Rename add_msg_issue_prefix.py to add_msg_issue_prefix_hook.py (#3)

--- hooks/add_msg_issue_prefix_hook.py
+++ hooks/add_msg_issue_prefix_hook.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 
 import argparse
-import sys
 import re
 import subprocess
 
@@ -14,37 +14,39 @@ def get_ticket_id_from_branch_name(branch):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("commit_msg_filepath")
+    parser.add_argument('commit_msg_filepath')
     parser.add_argument(
-        '-t', '--template', default="{} - ",
+        '-t', '--template', default='{} - ',
         help='Template to render ticket id into',
     )
     args = parser.parse_args()
     commit_msg_filepath = args.commit_msg_filepath
     template = args.template
 
-    branch = ""
+    branch = ''
     try:
-        branch = subprocess.check_output(["git","symbolic-ref", "--short", "HEAD"], universal_newlines=True).strip()
+        branch = subprocess.check_output(
+            ['git', 'symbolic-ref', '--short', 'HEAD'], text=True,
+        ).strip()
     except Exception as e:
         print(e)
 
     result = get_ticket_id_from_branch_name(branch)
-    issue_number = ""
+    issue_number = ''
 
     if result:
         issue_number = result.upper()
 
-    with open(commit_msg_filepath, "r+") as f:
+    with open(commit_msg_filepath, 'r+') as f:
         content = f.read()
-        content_subject = content.split("\n", maxsplit=1)[0].strip()
+        content_subject = content.split('\n', maxsplit=1)[0].strip()
         f.seek(0, 0)
         if issue_number and issue_number not in content_subject:
             prefix = template.format(issue_number)
-            f.write("{} {}".format(prefix, content))
+            f.write(f'{prefix} {content}')
         else:
             f.write(content)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     exit(main())
